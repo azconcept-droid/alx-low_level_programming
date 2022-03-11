cdd070c64fb5ee48612db4b20ef80a9e5df215d3
Yahaya Azeez <azconcept2016@gmail.com>
2022-03-11 12:40:22 -0800

    commit task 0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -6,9 +6,11 @@
 /* betty style doc for function main goes there */
 /**
  * main -Entry point
- * @n: holds value each time code is ran
+ * @n: holds random value 
  *
- * Description: this program generate a random number each time the program is ran
+ * Description: 
+ * this program generate a random number
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
@@ -17,11 +19,11 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
-	if (n>0)
-	  printf("%d is positive\n",n);
-	else if (n==0)
-	  printf("%d is zero\n",n);
-	else if(n<0)
-	  printf("%d is negative\n",n);
+	if (n > 0)
+	  printf("%d is positive\n", n);
+	else if (n == 0)
+	  printf("%d is zero\n", n);
+	else if (n < 0)
+	  printf("%d is negative\n", n);
 	return (0);
 }
