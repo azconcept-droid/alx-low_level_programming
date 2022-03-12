07479c6f03bd1f0950e4745f0489fe1088407d22
Yahaya Azeez <azconcept2016@gmail.com>
2022-03-11 17:47:52 -0800

    commit task 3

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -7,13 +7,13 @@
  * uppercase.
  * Return: 0
  */
-int main()
+int main(void)
 {
 	int i;
 
 	for (i = 97; i < 123; i++)
 		putchar(i);
-	for (i = 65; i < 111; i++)
+	for (i = 65; i < 91; i++)
 		putchar(i);
 	putchar(10);
 	return (0);
