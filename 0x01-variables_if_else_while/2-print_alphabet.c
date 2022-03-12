58c8686921f2b63d8556f2b37e9c5c1f8d1b43db
Yahaya Azeez <azconcept2016@gmail.com>
2022-03-11 17:27:18 -0800

    modify 2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - main block
+ * Description: Print alphabets in lower case
+ * Return: 0
+ */
+int main(void)
+{
+	for (int i = 97; i < 123; i++)
+		putchar(i);
+	putchar(10);
+	return (0);
+}
