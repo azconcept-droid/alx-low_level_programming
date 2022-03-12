54f4519fad521cee547c891b398941411e0f9bb7
Yahaya Azeez <azconcept2016@gmail.com>
2022-03-11 18:22:17 -0800

    commit task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ * Description: prints single digit
+ * numbers of base 10 starting
+ * from 0.
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 46; i < 58; i++)
+	       printf("%c", i);
+	putchar(10);
+	return (0);
+}
