83ef3b6c048c1e706dc4bddaaa099775872d2388
Yahaya Azeez <azconcept2016@gmail.com>
2022-03-15 21:38:40 +0000

    task 1 commit

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,18 +1,5 @@
 #include "main.h"
 /**
- * main - Entry point
- *
- * Description:  prints the alphabet, in lowercase,
- * followed by a new line.
- * Return: 0
- */
-int main(void)
-{
-	print_alphabet();
-	return (0);
-}
-
-/**
  * print_alphabet - prints alphabets
  * @i: ascii variable
  *
