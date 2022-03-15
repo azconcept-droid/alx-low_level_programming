cd5ab3bad965c76f384fdffca276592147b5b9c9
Yahaya Azeez <azconcept2016@gmail.com>
2022-03-15 23:55:47 +0000

    task 10 commit

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * add - add function
+ * @a: first value
+ * @b: second value
+ *
+ * Description: function that adds two
+ * integers and returns the result.
+ * Return: sum of both
+ */
+int add(int a, int b)
+{
+	int result;
+
+	result = a + b;
+
+	return (result);
+}
