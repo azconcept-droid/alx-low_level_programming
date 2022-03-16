0ed449f91e098c86a519c13ef38c5194e2e6d840
Yahaya Azeez <azconcept2016@gmail.com>
2022-03-16 11:15:39 +0000

    commit task 5

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,4 +1,3 @@
-#include "main.h"
 /**
  * _islower - lowercase finder
  * @c: argument
@@ -9,8 +8,5 @@
  */
 int _islower(int c)
 {
-	if (c > 96 && c < 123)
-		return (1);
-	else
-		return (0);
+	return ((c > 96 && c < 123));
 }
