b5ff9d88cad90850616028ab41670d64d2d2cfc5
Yahaya Azeez <azconcept2016@gmail.com>
2022-03-16 13:31:01 +0000

    correct task 7

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -11,6 +11,14 @@ int print_last_digit(int num)
 {
 	int ld = (num % 10);	/*ld: last digit*/
 
-	_putchar(ld + '0');
-	return (ld);
+	if (num > 0 || num == 0)
+	{
+		_putchar(ld + '0');	 /*if ld is +*/
+		return (ld);
+	}
+	else
+	{
+		_putchar((ld * (-1)) + '0');	/*if ld is -*/
+		return ((-1) * ld);
+	}
 }
