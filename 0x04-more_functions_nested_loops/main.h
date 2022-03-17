0f692472aea5237bf35713b262c4dbff9a465648
Yahaya Azeez <azconcept2016@gmail.com>
2022-03-17 23:43:24 +0000

    task 12 commit

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,5 +1,5 @@
-#ifndef HEADER_H
-#define HEADER_H
+#ifndef _MAIN_H_
+#define _MAIN_H_
 
 int _isupper(int c);
 int _isdigit(int c);
@@ -11,5 +11,6 @@ void print_line(int n);
 void print_diagonal(int n);
 void print_square(int size);
 void print_triangle(int size);
+void print_number(int n);
 
 #endif
