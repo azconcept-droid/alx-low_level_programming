ad512dc04ae91a4c1f673a839ed1c459e38c1d73
Yahaya Azeez <azconcept2016@gmail.com>
2022-03-22 00:49:34 +0000

    task 7 commit

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+ * puts_half - prints half of a string
+ * @str: string to be pass into the function
+ *
+ * Return: nothing
+ */
+void puts_half(char *str)
+{
+	int len = 0;
+	int j;
+
+	while (str[len] != '\0')
+	{
+		len++;
+	}
+
+	if ((len % 2) == 0)
+	{
+		for (j = (len / 2); str[j] != '\0'; j++)
+		{
+			printf("%c", str[j]);
+		}
+	}
+	else
+	{
+		for (j = ((len - 1) / 2); str[j] != '\0'; j++)
+		{
+			printf("%c", str[j]);
+		}
+
+	}
+}
