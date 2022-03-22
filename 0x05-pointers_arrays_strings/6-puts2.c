f4638c22d716636e98397cd34af862243470a68b
Yahaya Azeez <azconcept2016@gmail.com>
2022-03-22 00:37:24 +0000

    task 6 commit

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * puts2 - prints every other character
+ * @str: string to be printed
+ *
+ * Description: prints every other character
+ * except odd numbers
+ * Return: nothing
+ */
+void puts2(char *str)
+{
+	int j;
+
+	for (j = 0; str[j] != '\0'; j++)
+	{
+		if ((str[j] % 2) != 0)
+			continue;
+		_putchar(str[j]);
+	}
+	_putchar('\n');
+}
