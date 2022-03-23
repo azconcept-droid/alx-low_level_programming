04cc0ad7e4e105801048d62f4d4e6c8f32de02c6
Yahaya Azeez <azconcept2016@gmail.com>
2022-03-23 23:54:59 +0000

    random num gen

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ *
+ */
+int main(void)
+{
+	int i;
+
+	srand(time(NULL));
+	for (i = 0; i < 10; i++)
+		printf("%i\n", (rand() % 10));
+}
