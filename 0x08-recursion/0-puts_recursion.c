6ff0b926414f0409025ee37abc20f6be40ab1d0f
Yahaya Azeez <azconcept2016@gmail.com>
2022-03-31 16:15:01 -0700

    task 0 commit

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _puts_recursion - prints a string
+ * @s: pointer to the string
+ *
+ * Return: nothing
+ */
+void _puts_recursion(char *s)
+{
+	/*Base case*/
+	if (*s == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
+	/*Print character*/
+	_putchar(*s);
+	/*Go to the next address*/
+	_puts_recursion(s++);
+}
