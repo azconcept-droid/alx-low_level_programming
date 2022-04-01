8abb0187b8a247af6408b6be2e2b782c7d49e2c7
Yahaya Azeez <azconcept2016@gmail.com>
2022-03-31 17:31:57 -0700

    task 3 commit

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * factorial - calculate factorial of a number
+ * @n: the number
+ *
+ * Return: the factorial of n
+ */
+int factorial(int n)
+{
+	if (n == 0)
+		return (1);
+	return (n * factorial(n - 1));
+}
