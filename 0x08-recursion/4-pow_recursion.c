75a725b8ed03be50c5dab1d014fda323beb0b929
Yahaya Azeez <azconcept2016@gmail.com>
2022-03-31 17:53:15 -0700

    task 4 commit

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _pow_recursion - find power of a number
+ * @x: the number
+ * @y: the value of power
+ *
+ * Return: the power of x
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y == 0)
+		return (1);
+	if (y < 0)
+		return (-1);
+	return (x * _pow_recursion(x, y - 1));
+}
