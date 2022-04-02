4d9153cfc6541974ab6b486ab7761edf93c565ac
Yahaya Azeez <azconcept2016@gmail.com>
2022-04-02 10:38:23 +0000

    modified 2-strchr.c

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -10,16 +10,12 @@
  */
 char *_strchr(char *s, char c)
 {
-	int i;
+	unsigned int i = 0;
 
-	if (s == NULL)
-		return (NULL);
-	for (i = 0; s[i] != '\0'; i++)
-	{
-		if (s[i] == c)
-			return (&s[i]);
-	}
-	if (s[i] == c)
-		return (&s[i]);
-	return (NULL);
+	for (; *(s + i) != '\0'; i++)
+		if (*(s + i) == c)
+			return (s + i);
+	if (*(s + i) == c)
+		return (s + i);
+	return ('\0');
 }
