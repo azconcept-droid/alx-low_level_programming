ae849dccd01dfdd8b6d4503afab3c5b720f3699f
Yahaya Azeez <azconcept2016@gmail.com>
2022-04-03 03:32:52 -0700

    task 0 commit

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry point
+ * @argc: number of arguments
+ * @argv: name of each arguments
+ *
+ * Return: 0
+ */
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
