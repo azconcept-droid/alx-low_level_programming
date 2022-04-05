22e53814dcea2bb279a4b4c9363c09851110be6b
Yahaya Azeez <azconcept2016@gmail.com>
2022-04-05 21:54:56 +0000

    modify 2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -10,8 +10,6 @@ char *str_concat(char *s1, char *s2)
 	char *s;
 	int i = 0, j = 0, s1_len = 0, s2_len = 0;
 
-	if (s1 == NULL && s2 == NULL)
-		return ("");
 	if (s1 != NULL)
 		while (s1[s1_len])
 			s1_len++;
