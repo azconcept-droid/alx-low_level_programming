78d82cfb55f74ea7121ad6dbdc600e506a4d19a2
Yahaya Azeez <azconcept2016@gmail.com>
2022-04-08 07:17:36 -0700

    add 101-mul.c

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <ctype.h>
+#include <unistd.h>
+int _isdigit(int c);
+int _putchar(char c);
 /**
  * main - multiply two arguments pass to it
  * @argc: number of arguments
@@ -18,19 +20,50 @@ int main(int argc, char *argv[])
 	{
 		for (j = 0; argv[i][j] != '\0'; j++)
 		{
-			if (!isdigit(argv[i][j]))
+			if (!_isdigit(argv[i][j]))
 			{
-				printf("Error\n");
+				printf("Error");
+				_putchar('\n');
 				exit(98);
 			}
 		}
 	}
-		printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+		printf("%d\n", (argv[1] * argv[2]));
 	}
 	else
 	{
-		printf("Error\n");
+		printf("Error");
+		_putchar('\n');
 		exit(98);
 	}
 	return (0);
+}
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
+/**
+ * _isdigit - checks for a digit.
+ * @c: input.
+ *
+ * Return: 1 if c is a digit
+ * 0 otherwise.
+ */
+int _isdigit(int c)
+{
+	if ((c >= '0') && (c <= '9'))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
 }
