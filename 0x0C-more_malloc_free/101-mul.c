aad898df068d06e1485e433a326b7bfd06d5a1e9
Yahaya Azeez <azconcept2016@gmail.com>
2022-04-08 18:22:14 -0700

    advanced task-101 commit

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,7 +1,5 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
-int _putchar(char c);
 /**
  * main - multiply two arguments pass to it
  * @argc: number of arguments
@@ -11,40 +9,27 @@ int _putchar(char c);
  */
 int main(int argc, char *argv[])
 {
+	unsigned long mul;
 	int i, j;
 
-	if (argc == 3)
+	if (argc != 3)
 	{
-		for (i = 1; i < argc; i++)
+		printf("Error\n");
+		exit(98);
+	}
+	for (i = 1; i < argc; i++)
 	{
 		for (j = 0; argv[i][j] != '\0'; j++)
 		{
-			if (argv[i][j] < 48 || argv[i][j] > 57)
+			if (argv[i][j] > 57 || argv[i][j] < 48)
 			{
-				printf("Error");
-				_putchar('\n');
+				printf("Error\n");
 				exit(98);
 			}
 		}
+
 	}
-		printf("%lu\n", atol(argv[1]) * atol(argv[2]));
-	}
-	else
-	{
-		printf("Error");
-		_putchar('\n');
-		exit(98);
-	}
+	mul = atol(argv[1]) *  atol(argv[2]);
+	printf("%lu\n", mul);
 	return (0);
-}
-/**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
 }
