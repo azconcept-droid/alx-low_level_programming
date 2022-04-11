80c30a6962b2e3f0f9f4596db0d4cd18fa3d5b22
Yahaya Azeez <azconcept2016@gmail.com>
2022-04-11 15:13:24 -0700

    TASK 3

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,9 +1,10 @@
-#ifndef HEADER_H
-#define HEADER_H
+#ifndef HEADER
+#define HEADER
 
 #if x < 0
-#define ABS(x) ((-1) * (x))
-#elif x > 0
+#define ABS(x) (x * -1)
+#endif
+#if x > 0
 #define ABS(x) x
 #endif
 
