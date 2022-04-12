270cb59d51da36b21d63bfb5a2e7ff01f6506380
Yahaya Azeez <azconcept2016@gmail.com>
2022-04-12 05:12:56 -0700

    TASK 1

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,15 @@
+/**
+ * init_dog - initializes a variable of type struct dog
+ * @d:pointer to the address of struct dog
+ * @name: pointer to the name of dog
+ * @age: pointer to the age of dog
+ * @owner: pointer to the owner of dog
+ *
+ * Return: nothing
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = &age;
+	d->owner = owner;
+}
