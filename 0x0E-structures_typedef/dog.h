99500909057a5241ab9e519acc023f305e707598
Yahaya Azeez <azconcept2016@gmail.com>
2022-04-12 04:35:42 -0700

    add dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef HEADER
+#define HEADER
+/**
+ * struct dog - dog
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: name of owner
+ *
+ * Description: user define datatype dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
