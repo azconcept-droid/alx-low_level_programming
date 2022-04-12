c74a6076058644894fe16c85b2cbbf977b7db584
Yahaya Azeez <azconcept2016@gmail.com>
2022-04-12 16:12:58 -0700

    task 5

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -1,3 +1,5 @@
+#include "dog.h"
+#include <stdlib.h>
 /**
  * free_dog - free memory allocated memory
  * @d: pointer to the memory address
@@ -6,5 +8,10 @@
  */
 void free_dog(dog_t *d)
 {
-	free(d);
+	if (d)
+	{
+		free(d->name);
+		free(d->owner);
+		free(d);
+	}
 }
