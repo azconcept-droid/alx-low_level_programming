10ed55943031b1b45a1c68515a6ef2908b6e5a22
Yahaya Azeez <azconcept2016@gmail.com>
2022-04-13 12:51:47 +0000

    task 0 commit

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,1 +1,10 @@
-#
+#include "function_pointers.h"
+/**
+ * print_name - print name to standard output
+ * @name: name to print
+ * @f: pointer to function
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
