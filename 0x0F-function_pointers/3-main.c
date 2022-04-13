b37efe5d934e25fd754b768cf6917aebdd606ac4
Yahaya Azeez <azconcept2016@gmail.com>
2022-04-13 16:19:13 +0000

    modify 3-main.c 3-calc.h

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,1 +1,16 @@
-#
+#include "3-calc.h"
+/**
+ * main - Entry point
+ * @argc: number of command line arguments
+ * @argv: argument names
+ *
+ * Return: 0.
+ */
+int main(int argc, char argv[])
+{
+	if (argc == 4)
+	{
+
+	}
+
+}
