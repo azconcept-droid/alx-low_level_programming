9c96817389e1ead2091c944575a7f4b25f96d5d6
Yahaya Azeez <azconcept2016@gmail.com>
2022-04-15 17:00:40 +0000

    modify 1-print_numbers.c

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -13,10 +13,10 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	unsigned int i;
 
 	va_start(ap, n);
-	for (i = 1; i <= n; i++)
+	for (i = 0; i < n; i++)
 	{
-		printf("%d", va_arg(ap, const unsigned int));
-		if ((i != n) && separator)
+		printf("%d", va_arg(ap, int));
+		if ((i < n - 1) && separator)
 			printf("%s", separator);
 	}
 	printf("\n");
