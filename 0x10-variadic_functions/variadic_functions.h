491fb9b88d78a18f59292ce393fb0badd135a2b8
Yahaya Azeez <azconcept2016@gmail.com>
2022-04-15 16:13:41 +0000

    modify variadic_functions.h

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,1 +1,9 @@
-#
+#ifndef HEADER_
+#define HEADER_
+
+int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+
+#endif
