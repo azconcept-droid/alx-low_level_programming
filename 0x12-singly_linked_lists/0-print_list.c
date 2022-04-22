6c04dacae048b8c4cd19be1cb5b418995c22cf03
Yahaya Azeez <azconcept2016@gmail.com>
2022-04-22 14:49:00 +0000

    modify 0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -7,7 +7,17 @@
  */
 size_t print_list(const list_t *h)
 {
-	if (h->str == NULL)
-		printf("[%l] (nil)\n");
+	size_t count;
 
+	count = 0;
+	while (h != NULL)
+	{
+		if(h->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%i] %s\n", h->len, h->str);
+		h = h->next;
+		count++;
+	}
+	return (count);
 }
