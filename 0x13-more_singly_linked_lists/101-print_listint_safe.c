73f66e9b2343767bb931b8a34b7754de9f7677d4
Yahaya Azeez <azconcept2016@gmail.com>
2022-04-26 16:43:39 +0000

    function that prints a listint_t linked list safe version.

---
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,62 @@
+#include "lists.h"
+void free_list(listp_t **head);
+/**
+ * print_listint_safe - function that prints a listint_t linked list.
+ * @head: pointer to head of list
+ *
+ * Description: This function can print lists with a loop
+ * Return: the number of nodes in the list
+ */
+size_t print_listint_safe(const listint_t *head)
+{
+	size_t nnodes = 0;
+	listp_t *hptr, *new, *add;
+
+	hptr = NULL;
+	while (head != NULL)
+	{
+		new = malloc(sizeof(listp_t));
+		if (new == NULL)
+			exit(98);
+		new->p = (void *)head;
+		new->next = hptr;
+		hptr = new;
+		add = hptr;
+		while (add->next != NULL)
+		{
+			add = add->next;
+			if (head == add->p)
+			{
+				printf("-> [%p] %d\n", (void *)head, head->n);
+				free_list(&hptr);
+				return (nnodes);
+			}
+		}
+		printf("[%p] %d\n", (void *)head, head->n);
+		head = head->next;
+		nnodes++;
+	}
+	free_list(&hptr);
+	return (nnodes);
+}
+/**
+ * free_list - frees the linked list
+ * @head: pointer to head address
+ *
+ * Return: nothing
+ */
+void free_list(listp_t **head)
+{
+	listp_t *current, *temp;
+
+	if (head != NULL)
+	{
+		current = *head;
+		while ((temp = current) != NULL)
+		{
+			current = current->next;
+			free(temp);
+		}
+		*head = NULL;
+	}
+}
