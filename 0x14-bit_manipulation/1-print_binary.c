fc8f0fd758d677d36bc128ee3e2f53d9917aafd5
Yahaya Azeez <azconcept2016@gmail.com>
2022-04-29 01:42:16 -0700

    Add function thatprint binary

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_binary - print decimal in binary
+ * @n: number to be printed
+ *
+ * Return: nothing
+ */
+void print_binary(unsigned long int n)
+{
+	unsigned long int n_dup;
+
+	n_dup = n;
+
+	if (n >= 2)
+	{
+		print_binary(n >> 1);
+		_putchar((n_dup & 1) + '0');
+	}
+	else 
+	{
+		_putchar(n + '0');
+		return;
+	}
+}
