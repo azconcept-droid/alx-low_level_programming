f87eb10beed206d12db30693e001c25d780967a7
Yahaya Azeez <azconcept2016@gmail.com>
2022-05-04 12:17:14 +0000

    Add 0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -4,7 +4,7 @@
  * @filename: name of file
  * @letters: maximum number of chars
  *
- * Description: reads a text file and print    s
+ * Description: reads a text file and prints
  * it to the POSIX standard output.
  * Return: actual number of letters it could read and print
  */
@@ -20,9 +20,10 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	if (buffer == NULL)
 		return (0);
 	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
 	l_read = read(fd, buffer, letters);
-
-	if (fd == -1 || l_read == -1)
+	if (l_read == -1)
 		return (0);
 	l_write = write(STDOUT_FILENO, buffer, l_read);
 	if (l_write == -1)
