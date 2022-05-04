4ce397bda8c6aa0c3db3a9ae6fdc765e273420f7
Yahaya Azeez <azconcept2016@gmail.com>
2022-05-04 13:48:50 +0000

    Add 1-create_file.c

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * create_file - creates a file and write to it
+ * @filename: file to write to
+ * @text_content: text to read
+ *
+ * Return: 1 on success, -1 on failure (file can not be created,
+ * file can not be written, write “fails”, etc…)
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd, i;
+	ssize_t l_write;
+
+	if (filename == NULL)
+		return (-1);
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	if (fd == -1)
+		return (-1);
+	i = 0;
+	while (text_content[i] != '\0')	/*Calculate length of text_content*/
+		i++;
+	l_write = write(fd, text_content, i);
+	if (l_write == -1)
+		return (-1);
+	close(fd);
+	return (1);
+}
