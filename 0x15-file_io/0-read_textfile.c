121db3af0302d3cc2898637498c9c6c4931b63aa
Yahaya Azeez <azconcept2016@gmail.com>
2022-05-04 03:46:48 -0700

    Add Requiescat

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,9 +1,11 @@
 #include "main.h"
 /**
- * read_textfile - reads a text file and prints it to the POSIX standard output.
+ * read_textfile - reads a text file
  * @filename: name of file
  * @letters: maximum number of chars
  *
+ * Description: reads a text file and print    s
+ * it to the POSIX standard output.
  * Return: actual number of letters it could read and print
  */
 ssize_t read_textfile(const char *filename, size_t letters)
@@ -23,5 +25,5 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	l_write = write(STDOUT_FILENO, buffer, l_read);
 	if (l_write == -1)
 		return (0);
-	return (l_write);	
+	return (l_write);
 }
