c11b92848324c9595320fd4392ff0a0e9400f5d2
Yahaya Azeez <azconcept2016@gmail.com>
2022-05-04 15:05:22 +0000

    Add 1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -17,12 +17,19 @@ int create_file(const char *filename, char *text_content)
 	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	if (fd == -1)
 		return (-1);
-
+	if (text_content == NULL)
+	{
+		close(fd);
+		return (1);
+	}
 	while (text_content[i] != '\0')	/*Calculate length of text_content*/
 		i++;
 	l_write = write(fd, text_content, i);
 	if (l_write == -1)
+	{
+		close(fd);
 		return (-1);
+	}
 	close(fd);
 	return (1);
 }
