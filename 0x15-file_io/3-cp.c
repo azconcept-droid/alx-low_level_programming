da5026132634d3278a8e2fced03a0dd6707e7d0e
Yahaya Azeez <azconcept2016@gmail.com>
2022-05-05 17:00:39 +0000

    add 3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,15 +1,69 @@
 #include "main.h"
-
+/**
+ * check_fd_and_argv2 - check if file can be written into
+ * @fd: file descriptor passed
+ * @arg: argv passed
+ *
+ * Return: nothing.
+ */
+void check_fd_and_argv2(int fd, char *arg)
+{
+	if (fd == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write from file %s\n", arg);
+		exit(99);
+	}
+	if (arg == NULL)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to file %s\n", arg);
+		exit(99);
+	}
+}
+/**
+ * check_fd_and_argv1 - check if file is open
+ * @fd: file descriptor passed
+ * @arg: argv passed
+ *
+ * Return: nothing
+ */
+void check_fd_and_argv1(int fd, char *arg)
+{
+	if (fd == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", arg);
+		exit(98);
+	}
+	if (arg == NULL)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", arg);
+		exit(98);
+	}
+}
+/**
+ * check_close - check if file is close
+ * @fd_close: return value of close
+ * @fd: file descriptor
+ *
+ * Return: nothing.
+ */
+void check_close(int fd_close, int fd)
+{
+	if (fd_close == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
+		exit(100);
+	}
+}
 /**
  * main - copies the content of a file to another file.
  * @argc: number of command line arguments
- * #argv: argument vectors
+ * @argv: argument vectors
  *
  * Return: 0 if successful.
  */
 int main(int argc, char **argv)
 {
-	int fd_from, fd_to, fdfr_close, fdto_close;
+	int fd_from = 0, fd_to = 0, fdfr_close, fdto_close;
 	int fdfr_read, fdto_write;
 	char buffer[1024];
 
@@ -18,54 +72,27 @@ int main(int argc, char **argv)
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
-	if (argv[1] == NULL)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		exit(98);
-	}
+	check_fd_and_argv1(fd_from, argv[1]);	/*Check if argv[1] is not NULL*/
+
 	fd_from = open(argv[1], O_RDONLY);
-	if (fd_from == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		exit(98);
-	}
-	if (argv[2] == NULL)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't write to file %s\n", argv[2]);
-		exit(99);
-	}
+
+	check_fd_and_argv1(fd_from, argv[1]);	/*Check if fd_from is not -1*/
+
+	check_fd_and_argv2(fd_to, argv[2]);	/*Check if argv[2] is not NULL*/
+
 	fd_to = open(argv[2], O_CREAT | O_RDWR | O_TRUNC, 0664);
-	if (fd_to == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't write to file %s\n", argv[2]);
-		exit(99);
-	}
+
+	check_fd_and_argv2(fd_to, argv[2]);	/*Check if fd_to is not -1*/
+
 	while ((fdfr_read = read(fd_from, buffer, 1024)))
 	{
-		if (fdfr_read == -1)
-		{
-			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-			exit(98);
-		}
+		check_fd_and_argv1(fdfr_read, argv[1]);
 		fdto_write = write(fd_to, buffer, fdfr_read);
-		if (fdto_write == -1)
-		{
-			dprintf(STDERR_FILENO, "Error: Can't write to file %s\n", argv[2]);
-			exit(99);
-		}
+		check_fd_and_argv2(fdto_write, argv[2]);
 	}
-
 	fdto_close = close(fd_to);
-	if (fdto_close == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to);
-		exit(100);
-	}
+	check_close(fdto_close, fd_to);
 	fdfr_close = close(fd_from);
-	if (fdfr_close == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from);
-		exit(100);
-	}
+	check_close(fdfr_close, fd_from);
 	return (0);
 }
