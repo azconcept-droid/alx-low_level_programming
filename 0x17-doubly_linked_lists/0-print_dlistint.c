ce41d395ad4305e610d546562aecb5fd091fd9e4
Yahaya Azeez <azconcept2016@gmail.com>
2022-06-10 04:43:39 +0000

    Add code

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,9 +1,8 @@
 #include "lists.h"
-
 /**
  * print_dlistint - prints all the elements of a dlistint_t list.
  * @h: pointer to head
- * 
+ *
  * Return: the number of nodes
  */
 size_t print_dlistint(const dlistint_t *h)
