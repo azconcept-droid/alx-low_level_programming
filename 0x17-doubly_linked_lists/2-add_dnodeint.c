f400665d37681e775553ad7b6e89f529f99fefd5
Yahaya Azeez <azconcept2016@gmail.com>
2022-06-10 07:25:57 -0700

    Kill it

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -14,8 +14,17 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	if (new_node == NULL)
 		return (NULL);
 	new_node->n = n;
-	new_node->prev = NULL;
+
+	if (*head == NULL)
+	{
+		new_node->prev = NULL;
+		new_node->next = NULL;
+		*head = new_node;
+	}
+
+	(*head)->prev = new_node;
 	new_node->next = *head;
+	new_node->prev = NULL;
 	*head = new_node;
 
 	return (new_node);
