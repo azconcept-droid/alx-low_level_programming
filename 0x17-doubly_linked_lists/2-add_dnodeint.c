61dad46e85709bc43b2f5677c7e8bf35fe901ad5
Yahaya Azeez <azconcept2016@gmail.com>
2022-06-10 07:37:58 -0700

    Finish it

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -20,7 +20,7 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 		new_node->prev = NULL;
 		new_node->next = NULL;
 		*head = new_node;
-		return new_node;
+		return (new_node);
 	}
 
 	(*head)->prev = new_node;
