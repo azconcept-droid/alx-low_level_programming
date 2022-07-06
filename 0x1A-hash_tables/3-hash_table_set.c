23bbcd7c87d0a1f812b9f262316bfcd698add204
Yahaya <azconcept2016@gmail.com>
2022-07-06 17:01:22 +0100

    :white_check_mark: adds an element to the hash table.

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,12 +1,12 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_set - 
- * 
- * @ht: 
- * @key: 
- * @value: 
- * Return: 
+ * hash_table_set - adds an element to the hash table.
+ *
+ * @ht: hash table to add or update the key/value to
+ * @key: is the key
+ * @value: is the value associated with the key.
+ * Return: 1 if it succeeded, 0 otherwise
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
@@ -21,14 +21,14 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	value_dup = strdup(value);
 	if (value_dup == NULL)
 		return (0);
-	
+
 	for (i = index; ht->array[i]; i++)
 	{
 		if (strcmp(ht->array[i]->key, key) == 0)
 		{
 			free(ht->array[i]->value);
 			ht->array[i]->value = value_dup;
-			return(1);	
+			return (1);
 		}
 	}
 
@@ -43,7 +43,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	if (hash_node->key == NULL)
 	{
 		free(hash_node);
-		return(0);
+		return (0);
 	}
 	hash_node->value = value_dup;
 	hash_node->next = ht->array[index];
