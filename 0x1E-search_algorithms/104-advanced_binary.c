25be19b97052a1ef3521b40012632944707f3a73
Yahaya <azconcept2016@gmail.com>
2022-10-06 02:13:06 +0100

    feat: add relevant files and push some codes with bugs

--- 0x1E-search_algorithms/104-advanced_binary.c
+++ 0x1E-search_algorithms/104-advanced_binary.c
@@ -1,9 +1,24 @@
 #include "search_algos.h"
 
 /**
- * advanced_binary -  searches for a value in a sorted array of integers
+ * min - compare two numbers
+ * @first: first number
+ * @second: second number
+ * Return: the smallest number
+ */
+size_t min(size_t first, size_t second)
+{
+	if (first > second)
+		return (second);
+	else
+		return (first);
+}
+
+/**
+ * bin_search - searches for a value in a sorted array of integers
  * @array: pointer to the first element of the array to search in
- * @size: number of elements in array
+ * @L: Left boundary
+ * @R: Right Boundary
  * @value: value to search for
  *
  * Description: searches for a value in an array of integers
@@ -12,15 +27,13 @@
  * If it is not present in array or if array is NULL,
  * your function must return -1
  */
-int advanced_binary(int *array, size_t size, int value)
+int bin_search(int *array, size_t L, size_t R, int value)
 {
-	/*@L: Left boundary, @R: Right Boundary @m: middle index*/
-	size_t L = 0, R = size - 1, m;
+	/*@m: middle index*/
+	size_t m;
 
-	if (!array || size <= 0)
-		return (-1);
-
-	do {
+	if (R >= L)
+	{
 		m = (L + R) / 2;
 
 		printf("Searching in array: ");
@@ -32,12 +45,49 @@ int advanced_binary(int *array, size_t size, int value)
 		}
 		printf("\n");
 		if (array[m] < value)
-			L = m + 1;
+			return (bin_search(array, m + 1, R, value));
 		else if (array[m] > value)
-			R = m - 1;
+			return (bin_search(array, L, m - 1, value));
 		else
 			return (m);
-	} while (L <= R);
+	}
 
 	return (-1);
+}
+
+/**
+ * advanced_binary - searches for a value in an array of integers
+ * @array: pointer to the first element of the array to search in
+ * @size: number of elements in array
+ * @value: value to search for
+ *
+ * Description: searches for a value in an array of integers
+ * using the advanced binary search algorithm
+ * Return: index of searched value,
+ * If it is not present in array or if array is NULL,
+ * your function must return -1
+ */
+int advanced_binary(int *array, size_t size, int value)
+{
+	/*@mini: minimum, @mid: middle, @bound: exponential bound jump*/
+	size_t bound = 1, mini, mid;
+	int value_index;
+
+	if (!array || size <= 0)
+		return (-1);
+
+	if (array[0] == value)
+		return (0);
+
+	while (bound < size && array[bound] <= value)
+	{
+		bound *= 2;
+	}
+
+	mid = bound / 2;
+	mini = min(bound, size - 1);
+
+	value_index = bin_search(array, mid, mini, value);
+
+	return (value_index);
 }
---
+++ 0x1E-search_algorithms/105-jump_list.c
@@ -0,0 +1,67 @@
+#include <math.h>
+#include "search_algos.h"
+
+/**
+ * min - compare two numbers
+ * @first: first number
+ * @second: second number
+ * Return: the smallest number
+ */
+size_t min(size_t first, size_t second)
+{
+	if (first > second)
+		return (second);
+	else
+		return (first);
+}
+
+/**
+ * jump_search - searches for a value in an array of integers
+ * @array: pointer to the first element of the array to search in
+ * @size: number of elements in array
+ * @value: value to search for
+ *
+ * Description: searches for a value in an array of integers
+ * using the jump search algorithm
+ * Return: index of searched value,
+ * If it is not present in array or if array is NULL,
+ * your function must return -1
+ */
+listint_t *jump_list(listint_t *list, size_t size, int value)
+{
+	size_t prev = 0, step, new_prev;
+
+	if (!array)
+		return (-1);
+
+	step = sqrt(size);
+
+	while (array[min(step, size) - 1] < value)
+	{
+		/*printf("Value checked array[%li] = [%i]\n",  prev, array[prev]);*/
+		new_prev = prev;
+		prev = step;
+		step += sqrt(size);
+		if (prev >= size)
+		{
+			/*printf("Value found between indexes [%li] and [%li]\n",  new_prev, prev);*/
+			return (-1);
+		}
+	}
+
+	while (array[prev] < value)
+	{
+		/*printf("Value checked array[%li] = [%i]\n", prev, array[prev]);*/
+		new_prev = prev;
+		prev++;
+		if (prev == min(step, size))
+		{
+			return (-1);
+		}
+	}
+	/*printf("Value found between indexes [%li] and [%li]\n",  new_prev, prev);*/
+	if (array[prev] == value)
+		return (prev);
+
+	return (-1);
+}
