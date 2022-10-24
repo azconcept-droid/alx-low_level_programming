6d93f77990eb96fca581d3d5be07378f37f6770f
Yahaya <azconcept2016@gmail.com>
2022-10-24 02:50:17 +0100

    feat: add code to fix DOS error

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,6 +1,6 @@
-#include "lists.h"
+#include "list.h"
 /**
- * print_dlistint - prints all the elements of a dlistint_t list.
+ * print_dlistint - print all the elements of a dlistint_t list.
  * @h: pointer to head
  *
  * Return: the number of nodes
@@ -13,12 +13,4 @@ size_t print_dlistint(const dlistint_t *h)
 	current = h;
 	nodes = 0;
 
-	while (current != NULL)
-	{
-		printf("%i\n", current->n);
-		current = current->next;
-		nodes++;
-	}
-
-	return (nodes);
 }
